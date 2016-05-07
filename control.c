6a1035f1881db4cfd2775b62e43a536f326eda38
Nate W <nwoythal@gmail.com>
2016-05-07 00:20:42 -0600

    finished etch-a-sketch mode

--- control.c
+++ control.c
@@ -1,12 +1,44 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <sys/ioctl.h>
 #include <unistd.h>
 #include <string.h>
 
+#ifdef _WIN32
+    #include <windows.h>
+    void sleep(long time)
+    {
+        Sleep(time*1000); //windows uses ms to sleep, and defines the function with 'S'
+    }
+#elif __linux__
+    #include <sys/ioctl.h>
+#else
+    #error "Unrecognized compiler"
+#endif
+
+enum {SP_Tree, SP_You, SP_Cave};
+
 void wait()
 {
+}
+
+void draw(int x, int y, int type)
+{
+    switch(type) 
+    {
+        case SP_You:
+            mvaddch(y, x, 'o');
+            break;
+        case SP_Tree:
+            mvaddch(y, x, 'φ');
+            break;
+        case SP_Cave:
+            mvaddch(y, x, 'Ħ');
+            break;
+        default:
+            wait();
+            break;
+    }
 }
 
 int main(int argc, char* argv[])
@@ -14,23 +46,29 @@ int main(int argc, char* argv[])
     //variable declarations
     initscr();
     raw();
+    curs_set(0);
     struct winsize size;
     ioctl(0, TIOCGWINSZ, &size);
     int x_loc=size.ws_col/2; //start x
     int y_loc=size.ws_row/2; //start y
     char floor[size.ws_row][size.ws_col];
     keypad(stdscr, TRUE);
-    char you='o';
     unsigned short input='\0'; //Store in int because KEY_values are large, apparently.
-    char mode='\0';
-    if(strcmp(argv[1], "-e"))
+    char mode='\0'; //only supports 'e' for now.
+
+    //Check if etch a sketch argument was passed.
+    if(argc>1 && argv[1][1]=='e')
     {
+        curs_set(2);
         printw("Starting in etch-a-sketch mode.");
-        sleep(50000);
+        refresh();
+        sleep(2);
         clear();
         mode='e';
     }
+    
 
+    draw(x_loc, y_loc, SP_You);
     //Movement loop, exits on ESC press.
     while(input!=27)
     {
@@ -66,7 +104,7 @@ int main(int argc, char* argv[])
             clear();
         else
             refresh();
-        mvaddch(y_loc,x_loc,you);
+        draw(x_loc, y_loc, SP_You);
     }
     endwin();
     return 0;
